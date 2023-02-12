f5a4376d249ffee9f94ac2e2b2c1d88431f08f08
James Childers <james.childers@gmail.com>
2023-02-12 15:35:51 -0600

    get tmux default session names working

--- xdg_config/nvim/lua/plugins/statusline.lua
+++ xdg_config/nvim/lua/plugins/statusline.lua
@@ -4,13 +4,20 @@ return {
   dependencies = { "SmiteshP/nvim-navic" },
   config = function()
     local navic = require("nvim-navic")
+    local name_or_navic = function()
+      if navic.is_available() then
+      	return navic.get_location()
+      else
+	return 'filename'
+      end
+    end
     require("lualine").setup({
       sections = {
 	lualine_a = { 'mode' },
-	lualine_b = { 'diagnostics', 'filename' },
+	lualine_b = { name_or_navic },
 	lualine_c = { { navic.get_location, cond = navic.is_available } },
 	lualine_x = { },
-	lualine_y = { 'progress' },
+	lualine_y = { 'diagnostics' },
 	lualine_z = { 'location' }
       },
     })
